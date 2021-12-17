c676556fe444be0bb1e8ad08909407c7863c34b0
ALEXIABCN <alexiainthesky@gmail.com>
2021-12-17 12:52:31 +0100

    first commit

---
+++ src/test/java/org/factoriaf5/bftp2project4grupo6/Bftp2Project4Grupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2project4grupo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bftp2Project4Grupo6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
