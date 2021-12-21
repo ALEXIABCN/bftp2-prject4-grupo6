a9e84f4ca573b68e67baa1289cd490253bfa88c9
ALEXIABCN <alexiainthesky@gmail.com>
2021-12-21 14:24:58 +0100

    añadido Thymeleaf

--- src/main/java/org/factoriaf5/bftp2project4grupo6/controllers/HomeController.java
+++ src/main/java/org/factoriaf5/bftp2project4grupo6/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.factoriaf5.bftp2project4grupo6.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
